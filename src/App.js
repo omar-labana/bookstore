25970d0999c0299c44a295f7f372df499cfa377d
Omar Labana <omarlabana@outlook.com>
2021-08-21 21:58:00 +0200

    Move App component

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Hello BOOOK</h1>
-  );
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
+
+const App = () => (
+  <main className="">
+    <BooksForm />
+    <BooksList />
+  </main>
+);
+
+export default App;
