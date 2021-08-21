84dae1e009e0dcaa3e763b0c5cdc64e1efd771c6
Omar Labana <omarlabana@outlook.com>
2021-08-22 01:30:14 +0200

    Edit form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -3,11 +3,11 @@ import React from 'react';
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
 const renderCategories = (categories) => {
-  categories.map((category) => (
+  return categories.map((category) => (
     <option value={category} key={category}>{category}</option>
   ));
 };
-
+console.log(renderCategories(categories));
 const BookForm = () => (
   <div>
     <input type="text" id="title" placeholder="Title" />
